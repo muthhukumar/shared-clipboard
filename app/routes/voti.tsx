93d26e169e71cc87d65e8d29dcc129c208a14b59
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2022-03-05 08:15:54 +0000

    feat: fixed the voti input

--- app/routes/voti.tsx
+++ app/routes/voti.tsx
@@ -8,6 +8,8 @@ import {
   FormControl,
   FormErrorMessage,
   VStack,
+  useColorModeValue,
+  StackDivider,
 } from '@chakra-ui/react'
 import { User, Voti } from '@prisma/client'
 import { IoMdAdd } from 'react-icons/io'
@@ -22,15 +24,76 @@ import {
   MetaFunction,
   useActionData,
   useTransition,
+  Outlet,
+  ActionFunction,
+  redirect,
 } from 'remix'
 import { NoItems, Wrapper } from '~/components'
 import { authenticator } from '~/utils/auth.server'
 import { prisma } from '~/utils/prisma.server'
-import { ActionDataType } from './voti/new'
+import { z } from 'zod'
+import { formatFieldErrorsNew } from '~/utils'
 
 export const meta: MetaFunction = () => {
   return {
     title: 'Voti',
+  }
+}
+
+const VotiSchema = z.object({
+  title: z.string().min(5),
+})
+
+export type ActionDataType = {
+  values: z.infer<typeof VotiSchema> | Record<string, unknown>
+  errors: Record<
+    keyof z.infer<typeof VotiSchema>,
+    {
+      message: string
+      isInvalid: boolean
+    }
+  >
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const user = (await authenticator.isAuthenticated(request, {
+    failureRedirect: '/login',
+  })) as User
+
+  const formData = await request.formData()
+
+  const actionData: ActionDataType = {
+    values: {},
+    errors: {
+      title: { isInvalid: true, message: '' },
+    },
+  }
+
+  const votiContentData = {
+    title: formData.get('title'),
+  }
+
+  const votiValidationResult = VotiSchema.safeParse(votiContentData)
+
+  if (!votiValidationResult.success) {
+    actionData.values = { ...votiContentData }
+    actionData.errors = {
+      ...formatFieldErrorsNew(votiContentData, votiValidationResult.error.formErrors.fieldErrors),
+    }
+
+    return actionData
+  }
+
+  try {
+    await prisma.voti.create({
+      data: {
+        title: votiValidationResult.data.title,
+        userEmail: user.email,
+      },
+    })
+    return redirect(`/voti`)
+  } catch (err) {
+    return redirect('/')
   }
 }
 
@@ -80,25 +143,28 @@ export default function ClipbaordContent() {
 
   const actionData = useActionData<ActionDataType>()
 
+  const borderColor = useColorModeValue('gray.200', 'gray.700')
+
   return (
     <div className="flex w-full py-8">
       <Wrapper>
-        <HStack>
+        <Form
+          className="flex items-center justify-between w-full"
+          method="get"
+          onChange={(target) => submit(target.currentTarget)}
+        >
+          <InputGroup size="md">
+            {/* eslint-disable-next-line react/no-children-prop*/}
+            <InputLeftElement pointerEvents="none" children={<RiSearchLine color="gray.300" />} />
+            <Input pr="4.5rem" type="text" name="q" placeholder="Search..." />
+          </InputGroup>
+        </Form>
+
+        <div className="p-4 mt-4 border rounded-md">
           <Form
-            className="flex items-center justify-between w-2/5"
-            method="get"
-            onChange={(target) => submit(target.currentTarget)}
-          >
-            <InputGroup size="md">
-              {/* eslint-disable-next-line react/no-children-prop*/}
-              <InputLeftElement pointerEvents="none" children={<RiSearchLine color="gray.300" />} />
-              <Input pr="4.5rem" type="text" name="q" placeholder="Search..." />
-            </InputGroup>
-          </Form>
-          <Form
-            className="flex items-center justify-between w-full"
+            className="flex items-center justify-between w-full mb-8"
             method="post"
-            action="/voti/new"
+            action="/voti"
             key={transition.location?.key}
           >
             <FormControl isInvalid={actionData?.errors.title.isInvalid}>
@@ -114,38 +180,36 @@ export default function ClipbaordContent() {
               Add
             </Button>
           </Form>
-        </HStack>
-        <VStack alignItems={'flex-start'} mt="8">
-          {voties.map((voti, index) => {
-            return (
-              <div
-                key={voti.id}
-                className="flex items-center justify-between w-full px-4 py-2 border rounded-md"
-              >
-                <p className="flex items-center justify-center w-8 h-8 p-2 mr-4 font-bold border rounded-full">
-                  {index + 1}
-                </p>
-                <p className="font-bold">{voti.title}</p>
-                <HStack ml="auto" pl="2" spacing={4}>
-                  <HStack>
-                    <p className="flex items-center justify-center w-8 h-8 p-2 ml-2 font-bold border rounded-full">
-                      {voti.votes}
-                    </p>
-                    <p className="mr-2 font-bold">votes</p>
+          <VStack alignItems={'flex-start'} divider={<StackDivider borderColor={borderColor} />}>
+            {voties.map((voti, index) => {
+              return (
+                <div key={voti.id} className="flex items-center justify-between w-full rounded-md">
+                  <p className="flex items-center justify-center w-8 h-8 p-2 mr-4 font-bold border rounded-full">
+                    {index + 1}
+                  </p>
+                  <p className="font-bold">{voti.title}</p>
+                  <HStack ml="auto" pl="2" spacing={4}>
+                    <HStack>
+                      <p className="flex items-center justify-center w-8 h-8 p-2 ml-2 font-bold border rounded-full">
+                        {voti.votes}
+                      </p>
+                      <p className="mr-2 font-bold">votes</p>
+                    </HStack>
+                    <Form method="post" action={`/voti/${voti.id}/upvote`}>
+                      <IconButton aria-label="Upvote" type="submit" icon={<BsArrowUp />} />
+                    </Form>
+                    <Form method="post" action={`/voti/${voti.id}/downvote`}>
+                      <IconButton type="submit" aria-label="Down vote" icon={<BsArrowDown />} />
+                    </Form>
                   </HStack>
-                  <Form method="post" action={`/voti/${voti.id}/upvote`}>
-                    <IconButton aria-label="Upvote" type="submit" icon={<BsArrowUp />} />
-                  </Form>
-                  <Form method="post" action={`/voti/${voti.id}/downvote`}>
-                    <IconButton type="submit" aria-label="Down vote" icon={<BsArrowDown />} />
-                  </Form>
-                </HStack>
-              </div>
-            )
-          })}
-          {voties.length === 0 && <NoItems title="No Voties found. Please add some!!!" />}
-        </VStack>
+                </div>
+              )
+            })}
+            {voties.length === 0 && <NoItems title="No Voties found. Please add some!!!" />}
+          </VStack>
+        </div>
       </Wrapper>
+      <Outlet />
     </div>
   )
 }
