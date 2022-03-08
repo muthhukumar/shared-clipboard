00e55e5cd40c2b6095a4ecc27ee9dbeb513bfbe4
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2022-03-08 16:40:54 +0000

    ui improvements

--- app/components/wrapper.tsx
+++ app/components/wrapper.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
 
 export default function Wrapper({ children }: { children: React.ReactNode }) {
-  return <div className="container w-full max-w-5xl px-6 mx-auto">{children}</div>
+  return <div className="container w-full max-w-5xl px-4 mx-auto md:px-6">{children}</div>
 }
