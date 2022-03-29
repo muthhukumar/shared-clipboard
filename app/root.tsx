53c48988df5ce5161226d5d00604c77feee85638
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2022-03-29 18:01:28 +0530

    feat: fix gogoel analyticsg

--- app/root.tsx
+++ app/root.tsx
@@ -88,7 +88,7 @@ function Document({ children, title }: { children: React.ReactNode; title?: stri
         <Links />
       </head>
       <body>
-        {process.env.NODE_ENV !== 'development' ? null : (
+        {process.env.NODE_ENV === 'development' ? null : (
           <>
             <script
               async
