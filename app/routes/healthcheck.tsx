768e3ec66959c1436a4ce7256858c7bfbfbb4c73
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-09-24 10:16:29 +0530

    added prisma to healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,10 +1,12 @@
 import type { LoaderFunction } from '@remix-run/node'
+import { prisma } from '~/utils/prisma.server'
 
 export const loader: LoaderFunction = async ({ request }) => {
   const host = request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
 
   try {
     await Promise.all([
+      prisma.user.count(),
       fetch(`http://${host}`, { method: 'HEAD' }).then((r) => {
         if (!r.ok) return Promise.reject(r)
       }),
